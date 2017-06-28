ca2cf611a34d308bef08fdf5c4511314e9746d0b
Wolfson Liu <wolfsonliu@gmail.com>
2017-06-28 10:14:42 +0800

    ColorTools add color function

--- ColorTools/colortools.py
+++ ColorTools/colortools.py
@@ -1,30 +1,56 @@
+# -*-coding:utf-8-*-
+# import matplotlib as mpl
+# mpl.use('Gtk3Agg')
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
-def easycolor(x, n):
+cmaps = {
+    'Perceptually Uniform Sequential': [
+        'viridis', 'plasma', 'inferno', 'magma'
+    ],
+    'Sequential': [
+        'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+        'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+        'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'
+    ],
+    'Sequential (2)': [
+        'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+        'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+        'hot', 'afmhot', 'gist_heat', 'copper'
+    ],
+    'Diverging': [
+        'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+        'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'
+    ],
+    'Qualitative': [
+        'Pastel1', 'Pastel2', 'Paired', 'Accent',
+        'Dark2', 'Set1', 'Set2', 'Set3',
+        'tab10', 'tab20', 'tab20b', 'tab20c'
+    ],
+    'Miscellaneous': [
+        'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+        'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'hsv',
+        'gist_rainbow', 'rainbow', 'jet', 'nipy_spectral', 'gist_ncar'
+    ]
+}
+
+def easycolor(colormap, n):
     def func(scale):
         return colors.rgb2hex(
-            plt.get_cmap(x, n)(scale)
+            plt.get_cmap(colormap, n)(scale)
         )
     return func
 
-colorbrewer = {
-    'Blues':   easycolor('Blues', 10),
-    'BuGn':    easycolor('BuGn', 10),
-    'BuPu':    easycolor('BuPu', 10),
-    'GnBu':    easycolor('GnBu', 10),
-    'Greens':  easycolor('Greens', 10),
-    'Greys':   easycolor('Greys', 10),
-    'Oranges': easycolor('Oranges', 10),
-    'OrRd':    easycolor('OrRd', 10),
-    'PuBu':    easycolor('PuBu', 10),
-    'PuBuGn':  easycolor('PuBuGn', 10),
-    'PuRd':    easycolor('PuRd', 10),
-    'Purples': easycolor('Purples', 10),
-    'RdPu':    easycolor('RdPu', 10),
-    'Reds':    easycolor('Reds', 10),
-    'YlGn':    easycolor('YlGn', 10),
-    'YlGnBu':  easycolor('YlGnBu', 10),
-    'YlOrBr':  easycolor('YlOrBr', 10),
-    'YlOrRd':  easycolor('YlOrRd', 10)
-}
+colorbrewer = dict(
+    zip(
+        cmaps['Sequential'],
+        [ easycolor(x, 256) for x in cmaps['Sequential'] ]
+    )
+)
+
+def colorseries(colormap, n):
+    return [
+        colors.rgb2hex(x) for x in plt.get_cmap(colormap, n)(range(n))
+    ]
+
+####################
