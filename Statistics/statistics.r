d5523a57c2e6c87f56e6cfc57d19087a8568dc28
wolfsonliu <wolfsonliu@gmail.com>
2017-08-27 17:13:33 +0800

    edit statistics function

--- Statistics/statistics.r
+++ Statistics/statistics.r
@@ -1,13 +1,35 @@
+rowVars <- function(x) {
+    ## calculate row variables
+    nx <- dim(x)[2]
+    rvar <- rowSums((x - rowMeans(x)) ^ 2) / (nx - 1)
+    return(rvar)
+}
+
+
 leverage <- function(data) {
     ## calculate the laverage statistic
     then <- length(data)
     thess <- var(data) * (then - 1)
     themean <- mean(data)
-    h <- unlist(
+    l <- unlist(
         lapply(
             data,
             function(x) { 1 / then + (x - themean)^2 / thess}
         )
     )
-    return(h)
+    result <- list(
+        leverage=l,
+        mean=mean(l),
+        var=var(l)
+    )
+    return(result)
+}
+
+predict.regsubsets <- function(object, newdata, id, ...) {
+    ## used for regsubsets prediction
+    form <- as.formula(object$call[[2]])
+    mat <- model.matrix(form, newdata)
+    coefi <- coef(object, id=id)
+    xvars <- names(coefi)
+    mat[, xvars] %*% coefi
 }
